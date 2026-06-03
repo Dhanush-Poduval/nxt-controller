787d92ee0a64fba6557481f0d4230b7705e89d7e
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-06-03 23:20:28 +0530

    feat:tested the diff drive communication with teleop

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -1,23 +1,32 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterFile
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
 
-    robot_desc = Command([
-        "xacro ",
-        os.path.join(
-            get_package_share_directory("nxt_hardware"),
-            "models/robot.urdf.xacro"
-        )
-    ])
+    xacro_file = os.path.join(
+        get_package_share_directory("nxt_hardware"),
+        "models",
+        "robot.urdf.xacro"
+    )
+
+    robot_desc = Command(["xacro ", xacro_file])
+
+    controller_yaml = os.path.join(
+        get_package_share_directory("nxt_hardware"),
+        "config",
+        "diff_drive_controller.yaml"
+    )
 
     rsp = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_desc}]
+        parameters=[{"robot_description": robot_desc}],
+        output="screen"
     )
 
     control_node = Node(
@@ -25,10 +34,7 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[
             {"robot_description": robot_desc},
-            os.path.join(
-                get_package_share_directory("nxt_hardware"),
-                "config/diff_drive_controller.yaml"
-            )
+            ParameterFile(controller_yaml, allow_substs=True)
         ],
         output="screen"
     )
