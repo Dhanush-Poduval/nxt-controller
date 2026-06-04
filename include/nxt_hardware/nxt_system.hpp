112bb050d157a5150a841f9fd478b26aa3c7a2fc
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-06-04 15:09:04 +0530

    feat:declares the hardware interface functions and made the header file

---
+++ include/nxt_hardware/nxt_system.hpp
@@ -0,0 +1,30 @@
+#pragma once 
+#include <vector>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/hardware_info.hpp"
+
+namespace nxt_hardware {
+  class NXTSystem: public hardware_interface::SystemInterface{
+    public:
+      hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+      std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+      std::vector<hardware_interface::CommandInterface> export_command_interface() override;
+      hardware_interface::return_type read(
+        const rclcpp::Time & time,
+        const rclcpp::Duration & period) override;
+
+      hardware_interface::return_type write(
+        const rclcpp::Time & time,
+        const rclcpp::Duration & period) override;
+    private:
+      double left_pos=0.0;
+      double right_pos=0.0;
+      double left_vel=0.0;
+      double right_vel=0.0;
+      double left_cmd=0.0;
+      double right_cmd=0.0;
+  };
+}
