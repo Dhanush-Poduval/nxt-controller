e1e73e4ebe326d451f4775af988a045e3e149be4
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-06-04 16:54:03 +0530

    feat:tested the code and topics working etc the read and write

--- src/nxt_system.cpp
+++ src/nxt_system.cpp
@@ -1,9 +1,9 @@
 #include "nxt_hardware/nxt_system.hpp"
 #include <cmath>
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
-#include <vector.h>
+#include <vector>
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
-using namespace std;
 namespace nxt_hardware {
   hardware_interface::CallbackReturn NXTSystem::on_init(const hardware_interface::HardwareInfo & info){
     if(hardware_interface::SystemInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS){
@@ -11,16 +11,16 @@ namespace nxt_hardware {
     };
     return hardware_interface::CallbackReturn::SUCCESS;
   };
-  vector<hardware_interface::StateInterface>NXTSystem::export_state_interfaces(){
-    vector<hardware_interface::StateInterface> state_interfaces;
+  std::vector<hardware_interface::StateInterface>NXTSystem::export_state_interfaces(){
+    std::vector<hardware_interface::StateInterface> state_interfaces;
     state_interfaces.emplace_back("left_wheel_joint",hardware_interface::HW_IF_POSITION,&left_pos);
     state_interfaces.emplace_back("left_wheel_joint",hardware_interface::HW_IF_VELOCITY,&left_vel);
     state_interfaces.emplace_back("right_wheel_joint",hardware_interface::HW_IF_POSITION,&right_pos);
     state_interfaces.emplace_back("right_wheel_joint",hardware_interface::HW_IF_VELOCITY,&right_vel);
     return state_interfaces;
   };
-  vector<hardware_interface::CommandInterface>NXTSystem::export_command_interfaces(){
-    vector<hardware_interface::CommandInterface> command_interfaces;
+  std::vector<hardware_interface::CommandInterface>NXTSystem::export_command_interfaces(){
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
     command_interfaces.emplace_back("left_wheel_joint",hardware_interface::HW_IF_VELOCITY,&left_cmd);
     command_interfaces.emplace_back("right_wheel_joint",hardware_interface::HW_IF_VELOCITY,&right_cmd);
     return command_interfaces;
@@ -33,6 +33,7 @@ namespace nxt_hardware {
     return hardware_interface::return_type::OK;
   };
   hardware_interface::return_type NXTSystem::write(const rclcpp::Time & /*time */, const rclcpp::Duration & /*period */){
+    std::cout<<"left_cmd : "<<left_cmd<<" right_cmd : "<<right_cmd<<std::endl;
     return hardware_interface::return_type::OK;
   };
 
